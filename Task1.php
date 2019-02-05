54fb86fbb350967a04d7499e60be5a0559230ff7
Shreya Patel <shreya984@hotmail.com>
2019-02-04 20:45:28 -0700

    no changes

--- Task1.php
+++ Task1.php
@@ -14,16 +14,16 @@ $json = file_get_contents('CalgarySchools.geojson');
 //Now decode the JSON into an associative array
 $myArr = json_decode($json, true);
 
-// print out the array, nicely formatted 
+// print out the array, nicely formatted
 echo "<hr>DECODED JSON:<hr>";
-echo "<pre>" . print_r($myArr, true) . "</pre>"; 
+echo "<pre>" . print_r($myArr, true) . "</pre>";
 
-//re-encode the arary as json 
+//re-encode the arary as json
 $newJSON = json_encode($myArr, JSON_PRETTY_PRINT);
 
 //print it out again, nicely formatted
 echo "<hr>NEWLY RE-ENCODED JSON:<hr>";
-echo "<pre>" . print_r($newJSON, true) . "</pre>"; 
+echo "<pre>" . print_r($newJSON, true) . "</pre>";
 
 //write json to a file
 //NOTE: had to run 'chmod 777 Lab2' in command line to allow write-permissions, where Lab2 is the name of this directory
