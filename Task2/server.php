fc7d19e6be6b1019d0a748a82cd09a435a530af6
Ross Bartlett <rossjbartlett@gmail.com>
2019-02-05 15:34:43 -0700

    renamed Task2 files, commented out the file-saving

--- Task2/server.php
+++ Task2/server.php
@@ -22,8 +22,9 @@ $newJSON = json_encode($myArr, JSON_PRETTY_PRINT);
 echo "<hr>NEWLY RE-ENCODED JSON:<hr>";
 echo "<pre>" . print_r($newJSON, true) . "</pre>"; 
 
-//write json to a file
+//write json to a file (optional)
 //NOTE: had to run 'chmod 777 Lab2' in command line to allow write-permissions, where Lab2 is the name of this directory
+/*
 $outputFile = 'results.geojson';
 if(file_put_contents($outputFile, $newJSON)) {
     echo '<hr> Data successfully saved';
@@ -31,6 +32,7 @@ if(file_put_contents($outputFile, $newJSON)) {
 else {
     echo "error saving JSON file";
 }
+*/
 
 
 ?>
