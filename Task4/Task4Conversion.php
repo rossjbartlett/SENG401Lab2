e3858d64dc2a28d90d9ef8aedfb35100005113c7
Ross Bartlett <rossjbartlett@gmail.com>
2019-02-07 11:59:23 -0700

    cleaned up Task4Conversion file

--- Task4/Task4Conversion.php
+++ Task4/Task4Conversion.php
@@ -10,13 +10,14 @@ $quadrant1 = "";
 $quadrant2 ="";
 $bearing = "";
 $geodesic= "";
-$null = FALSE;
-$outRange = FALSE;
-$show = FALSE;
 
 //check if any field was left blank or if there was non numeric values
 if ((empty($latitude1) && $latitude1 != 0) || (empty($latitude2)&& $latitude2 != 0) || (empty($longitude1) && $longitude1 != 0) || (empty($longitude2) && $longitude2!=0) || !is_numeric($latitude1) || !is_numeric($latitude2) || !is_numeric($longitude1) || !is_numeric($longitude2)){
-  $null = TRUE;
+  $outRange = FALSE;
+  echo "<script type='text/javascript'>
+      alert('A field was left blank or had invalid characters, please try again!!')
+      window.location = 'Task4.html';</script>";
+  die(); //dont do anything else.
 }
 
 //convert the points from string to float
@@ -27,90 +28,72 @@ $longitude2 = (float)$longitude2;
 
 //check if any field was out of longitude or latitude range
 if (($latitude1>90 || $latitude1<-90) || ($latitude2>90 || $latitude2<-90) || ($longitude1>180 || $longitude1<-180) || ($longitude2>180 || $longitude2<-180)){
-  $outRange = TRUE;
+  //inputs are out of range
+  echo "<script type='text/javascript'>
+      alert('Parameters not in range, please try again! Latitudes should be between -90 and +90 and longitudes between -180 and +180')
+      window.location = 'Task4.html';</script>";
+  die();
 }
 
-
-if (!$outRange && !$null){
-  //quadrant of point 1
-  if ($latitude1 > 0 && $longitude1 > 0){
-    $quadrant1 = 1;
-  }
-  else if ($latitude1 < 0 && $longitude1 > 0){
-    $quadrant1 = 4;
-  }
-  else if ($latitude1 < 0 && $longitude1 < 0){
-    $quadrant1 = 3;
-  }
-  else if ($latitude1 > 0 && $longitude1 < 0){
-    $quadrant1 = 2;
-  }
-  else{
-    $quadrant1 = "NA - point is on an axis";
-  }
-
-  //quadrant of point 2
-  if ($latitude2 > 0 && $longitude2 > 0){
-    $quadrant2 = 1;
-  }
-  else if ($latitude2 < 0 && $longitude2 > 0){
-    $quadrant2 = 4;
-  }
-  else if ($latitude2 < 0 && $longitude2 < 0){
-    $quadrant2 = 3;
-  }
-  else if ($latitude2 > 0 && $longitude2 < 0){
-    $quadrant2 = 2;
-  }
-  else{
-    $quadrant2 = "NA - point is on an axis";
-  }
-
-  //convert the degree values to radians
-  $latitude1 = deg2rad($latitude1);
-  $longitude1 = deg2rad($longitude1);
-  $latitude2 = deg2rad($latitude2);
-  $longitude2 = deg2rad($longitude2);
-
-  //bearing
-  $x = cos($latitude2)*sin(($longitude2-$longitude1));
-  $y = cos($latitude1)*sin($latitude2) - sin($latitude1)*cos($latitude2)*cos(($longitude2-$longitude1));
-  $bearing = atan2($x, $y);
-
-  //geodesic distance
-  $dlong = $longitude2 - $longitude1;
-  $dlat = $latitude2 - $latitude1;
-  $a = (sin($dlat/2)*sin($dlat/2)) + (cos($latitude1)*cos($latitude2)*(sin($dlong/2)*sin($dlong/2)));
-  $c = 2*asin(min(1, sqrt($a)));
-  $d = 6373*$c;     //6373 is the spherical earth radius
-  $geodesic = $d;
-
-  $show = TRUE;
-
+//get quadrant of point 1
+if ($latitude1 > 0 && $longitude1 > 0){
+  $quadrant1 = 1;
+}
+else if ($latitude1 < 0 && $longitude1 > 0){
+  $quadrant1 = 4;
+}
+else if ($latitude1 < 0 && $longitude1 < 0){
+  $quadrant1 = 3;
+}
+else if ($latitude1 > 0 && $longitude1 < 0){
+  $quadrant1 = 2;
+}
+else{
+  $quadrant1 = "NA - point is on an axis";
 }
 
-// appropriate error messages in javascript alerts
-if ($null){
-  $null = FALSE;
-  $outRange = FALSE;
-  echo "<script type='text/javascript'>alert('A field was left blank or had invalid characters, please try again!!')
-  window.location = 'Task4.html';</script>";
+//get quadrant of point 2
+if ($latitude2 > 0 && $longitude2 > 0){
+  $quadrant2 = 1;
 }
-else if($outRange)
-{
-  $null = FALSE;
-  $outRange = FALSE;
-  echo "<script type='text/javascript'>alert('Parameters not in range, please try again! Latitudes should be between -90 and +90 and longitudes between -180 and +180')
-  window.location = 'Task4.html';</script>";
+else if ($latitude2 < 0 && $longitude2 > 0){
+  $quadrant2 = 4;
 }
-else if ($show){
-  $show = FALSE;
-  echo "<hr>RESULTS:<hr>";
-  echo "<br>Quadrant of point 1: " .$quadrant1;
-  echo "<br>Quadrant of point 2: " .$quadrant2;
-  echo "<br>Bearing between points: " .round(rad2deg($bearing), 2 ,PHP_ROUND_HALF_UP). " degrees";
-  echo "<br>Geodesic distance between points: " .round($geodesic, 2 ,PHP_ROUND_HALF_UP). " km";
+else if ($latitude2 < 0 && $longitude2 < 0){
+  $quadrant2 = 3;
 }
+else if ($latitude2 > 0 && $longitude2 < 0){
+  $quadrant2 = 2;
+}
+else{
+  $quadrant2 = "NA - point is on an axis";
+}
+
+//convert the degree values to radians
+$latitude1 = deg2rad($latitude1);
+$longitude1 = deg2rad($longitude1);
+$latitude2 = deg2rad($latitude2);
+$longitude2 = deg2rad($longitude2);
+
+//bearing
+$x = cos($latitude2)*sin(($longitude2-$longitude1));
+$y = cos($latitude1)*sin($latitude2) - sin($latitude1)*cos($latitude2)*cos(($longitude2-$longitude1));
+$bearing = atan2($x, $y);
+
+//geodesic distance
+$dlong = $longitude2 - $longitude1;
+$dlat = $latitude2 - $latitude1;
+$a = (sin($dlat/2)*sin($dlat/2)) + (cos($latitude1)*cos($latitude2)*(sin($dlong/2)*sin($dlong/2)));
+$c = 2*asin(min(1, sqrt($a)));
+$d = 6373*$c;     //6373 is the spherical earth radius
+$geodesic = $d;
+
+//show results
+echo "<hr>RESULTS:<hr>";
+echo "<br>Quadrant of point 1: " .$quadrant1;
+echo "<br>Quadrant of point 2: " .$quadrant2;
+echo "<br>Bearing between points: " .round(rad2deg($bearing), 2 ,PHP_ROUND_HALF_UP). " degrees";
+echo "<br>Geodesic distance between points: " .round($geodesic, 2 ,PHP_ROUND_HALF_UP). " km";
 
 ?>
 
